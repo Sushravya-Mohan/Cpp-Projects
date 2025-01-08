ceda7cea4df5ce3bb9bf47b2a3dbbe47542fec13
Sushravya-Mohan <67335303+Sushravya-Mohan@users.noreply.github.com>
2025-01-08 16:58:55 +0000

    fav num

---
+++ 4-Basics/FavNum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int favorite_number;
+
+    cout << "Enter your favourite number between 1 and 100" << endl;
+    cin >> favorite_number;
+    cout << "Amazing! That's my favourite number too!" << endl;
+
+    return 0;
+}
